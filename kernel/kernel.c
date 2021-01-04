d7afb84a1661ba392b055e5c68b4bf5b30251438
tkyymmt <tkyymmt0x0@gmail.com>
2021-01-04 14:58:31 +0000

    commit

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,20 +1,16 @@
-/*
 #include "fb.h"
 #include "fbcon.h"
 #include "font.h"
-*/
 
 
 void start_kernel(struct framebuffer *fb)
 {
-    /*
     fb_init(fb);
     set_fg(255, 255, 255);
     set_bg(0, 0, 0);
     clear_screen();
 
     puts("HELLO WORLD!");
-    */
 
     while (1);
 }
